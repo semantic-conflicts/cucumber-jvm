2c09701fd28b014269d5b553057c625d45b17538
semantic conflicts <semantic.conflicts@gmail.com>
2020-05-25 15:18:05 -0300

    'Trigger build #1eb7936c19790c191e5087883f94ef77dd01f6f2'

--- cuke4duke/src/main/java/cuke4duke/internal/java/JavaLanguage.java
+++ cuke4duke/src/main/java/cuke4duke/internal/java/JavaLanguage.java
@@ -1,110 +1,104 @@
 package cuke4duke.internal.java;
-
 import cuke4duke.*;
 import cuke4duke.internal.language.ProgrammingLanguage;
 import cuke4duke.internal.language.StepDefinition;
 import cuke4duke.internal.language.StepMother;
 import cuke4duke.internal.language.LanguageMixin;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
-
 public class JavaLanguage extends ProgrammingLanguage {
-    private final ObjectFactory objectFactory;
-    private final LanguageMixin languageMixin;
-
-    public JavaLanguage(LanguageMixin languageMixin) throws Throwable {
-        this.languageMixin = languageMixin;
-        String className = System.getProperty("cuke4duke.objectFactory");
-        if(className == null) {
-            throw new RuntimeException("Missing system property: cuke4duke.objectFactory");
-        }
-        Class<ObjectFactory> ofc = (Class<ObjectFactory>) Thread.currentThread().getContextClassLoader().loadClass(className);
-        Constructor<ObjectFactory> ctor = ofc.getConstructor();
-        try {
-            objectFactory = ctor.newInstance();
-        } catch(InvocationTargetException e) {
-            throw e.getTargetException();
-        }
+  public ObjectFactory objectFactory;
+  public LanguageMixin languageMixin;
+  public JavaLanguage(  LanguageMixin languageMixin) throws Throwable {
+    this.languageMixin=languageMixin;
+    String className=System.getProperty("cuke4duke.objectFactory");
+    if (className == null) {
+      throw new RuntimeException("Missing system property: cuke4duke.objectFactory");
     }
-
-    protected void load(String java_file) throws ClassNotFoundException {
-        Class clazz = loadClass(java_file);
-        registerStepDefinitionsFor(clazz);
-        objectFactory.addClass(clazz);
+    Class<ObjectFactory> ofc=(Class<ObjectFactory>)Thread.currentThread().getContextClassLoader().loadClass(className);
+    Constructor<ObjectFactory> ctor=ofc.getConstructor();
+    try {
+      objectFactory=ctor.newInstance();
     }
-
-    public void begin_scenario() {
-        objectFactory.newWorld();
+ catch (    InvocationTargetException e) {
+      throw e.getTargetException();
     }
-
-    public void end_scenario() {
-        objectFactory.dispose();
+  }
+  public void load(  String java_file) throws ClassNotFoundException {
+    Class clazz=loadClass(java_file);
+    registerStepDefinitionsFor(clazz);
+    objectFactory.addClass(clazz);
+  }
+  public void begin_scenario(){
+    objectFactory.newWorld();
+  }
+  public void end_scenario(){
+    objectFactory.dispose();
+  }
+  public Class loadClass(  String javaPath) throws ClassNotFoundException {
+    String withoutExt=javaPath.substring(0,javaPath.length() - ".java".length());
+    String[] pathElements=withoutExt.split("\\/");
+    String className=null;
+    for (int i=pathElements.length - 1; i >= 0; i--) {
+      if (className == null) {
+        className=pathElements[i];
+      }
+ else {
+        className=pathElements[i] + "." + className;
+      }
+      try {
+        return Thread.currentThread().getContextClassLoader().loadClass(className);
+      }
+ catch (      ClassNotFoundException ignore) {
+      }
     }
-
-    private Class loadClass(String javaPath) throws ClassNotFoundException {
-        String withoutExt = javaPath.substring(0, javaPath.length() - ".java".length());
-        String[] pathElements = withoutExt.split("\\/");
-        String className = null;
-        for(int i = pathElements.length - 1; i >= 0; i--) {
-            if(className == null) {
-                className = pathElements[i];
-            } else {
-                className = pathElements[i] + "." + className;
-            }
-            try {
-                return Thread.currentThread().getContextClassLoader().loadClass(className);
-            } catch (ClassNotFoundException ignore) {
-            }
-        }
-        throw new ClassNotFoundException("Couldn't determine class from file: " + javaPath);
+    throw new ClassNotFoundException("Couldn't determine class from file: " + javaPath);
+  }
+  public void registerStepDefinitionsFor(  Class clazz){
+    for (    Method method : clazz.getMethods()) {
+      registerStepDefinitionMaybe(method);
+      registerBeforeMaybe(method);
+      registerAfterMaybe(method);
     }
-
-    private void registerStepDefinitionsFor(Class clazz) {
-        for (Method method : clazz.getMethods()) {
-            registerStepDefinitionMaybe(method);
-            registerBeforeMaybe(method);
-            registerAfterMaybe(method);
-        }
+  }
+  public void registerStepDefinitionMaybe(  Method method){
+    String regexpString=null;
+    if (method.isAnnotationPresent(Given.class)) {
+      regexpString=method.getAnnotation(Given.class).value();
     }
-
-    private void registerStepDefinitionMaybe(Method method) {
-        String regexpString = null;
-        if (method.isAnnotationPresent(Given.class)) {
-            regexpString = method.getAnnotation(Given.class).value();
-        } else if (method.isAnnotationPresent(When.class)) {
-            regexpString = method.getAnnotation(When.class).value();
-        } else if (method.isAnnotationPresent(Then.class)) {
-            regexpString = method.getAnnotation(Then.class).value();
-        }
-        if (regexpString != null) {
-            StepDefinition stepDefinition = new JavaStepDefinition(this, method, regexpString);
-            addStepDefinition(stepDefinition);
-        }
+ else     if (method.isAnnotationPresent(When.class)) {
+      regexpString=method.getAnnotation(When.class).value();
     }
-
-    private void registerBeforeMaybe(Method method) {
-        if (method.isAnnotationPresent(Before.class)) {
-            List<String> tagNames = Arrays.asList(method.getAnnotation(Before.class).value().split(","));
-            languageMixin.add_hook("before", new JavaHook(tagNames, method, this));
-        }
+ else     if (method.isAnnotationPresent(Then.class)) {
+      regexpString=method.getAnnotation(Then.class).value();
     }
-
-    private void registerAfterMaybe(Method method) {
-        if (method.isAnnotationPresent(After.class)) {
-            List<String> tagNames = Arrays.asList(method.getAnnotation(After.class).value().split(","));
-            languageMixin.add_hook("after", new JavaHook(tagNames, method, this));
-        }
+    if (regexpString != null) {
+      StepDefinition stepDefinition=new JavaStepDefinition(this,method,regexpString);
+      addStepDefinition(stepDefinition);
     }
-
-    Object getTarget(Class<?> type) {
-        Object target = objectFactory.getComponent(type);
-        if(target == null) {
-            throw new NullPointerException("Couldn't find object for type " + type);
-        }
-        return target;
+  }
+  public void registerBeforeMaybe(  Method method){
+    if (method.isAnnotationPresent(Before.class)) {
+      List<String> tagNames=Arrays.asList(method.getAnnotation(Before.class).value().split(","));
+      languageMixin.add_hook("before",new JavaHook(tagNames,method,this));
     }
+  }
+  public void registerAfterMaybe(  Method method){
+    if (method.isAnnotationPresent(After.class)) {
+      List<String> tagNames=Arrays.asList(method.getAnnotation(After.class).value().split(","));
+      languageMixin.add_hook("after",new JavaHook(tagNames,method,this));
+    }
+  }
+  Object getTarget(  Class<?> type){
+    Object target=objectFactory.getComponent(type);
+    if (target == null) {
+      throw new NullPointerException("Couldn't find object for type " + type);
+    }
+    return target;
+  }
+  public JavaLanguage(){
+  }
 }
